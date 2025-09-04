c96198cd69583aa280855294448dbca1da7c121a
kwesimain <opokulive@gmail.com>
2025-09-04 05:19:23 +0000

    updated CORS

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ process.on('unhandledRejection', (err) => {
 
 // Middleware
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production' ? 'https://raffle-hub.vercel.app' : 'http://localhost:3000',
+  origin: process.env.NODE_ENV === 'production' ? 'https://raffle-frontend-xi.vercel.app' : 'http://localhost:3000',
   exposedHeaders: ['X-Ticket-Number'],
 }));
 app.use(express.json());
